82c812b8a22b83ceaba3417f2a32a506afda932d
gavin <gavinklfong@gmail.com>
2025-12-01 14:31:24 +0000

    update

--- src/main/java/com/example/LinkedListBenchmark.java
+++ src/main/java/com/example/LinkedListBenchmark.java
@@ -4,22 +4,23 @@ import org.openjdk.jmh.annotations.*;
 
 import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.util.random.RandomGenerator;
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @State(Scope.Thread)
 public class LinkedListBenchmark {
 
-    @Param({"10000000"})
+    @Param({"100"})
     public int size;
 
     private List<Integer> linkedList;
-    private Random random;
+    private RandomGenerator randomGenerator;
 
     @Setup(Level.Trial)
     public void setup() {
+        randomGenerator = RandomGenerator.getDefault();
         linkedList = new LinkedList<>();
-        random = new Random(42);
 
         for (int i = 0; i < size; i++) {
             linkedList.add(i);
@@ -28,21 +29,52 @@ public class LinkedListBenchmark {
 
     // =============== ADDING ====================
     @Benchmark
-    public List<Integer> linkedListAdd() {
-        List<Integer> list = new LinkedList<>();
-        for (int i = 0; i < size; i++) list.add(i);
-        return list;
+    public Integer linkedListAppend() {
+        int num = randomGenerator.nextInt(100, 1000);
+        linkedList.add(num);
+        return num;
+    }
+
+    @Benchmark
+    public Integer linkedListInsert() {
+        int num = randomGenerator.nextInt(100, 1000);
+        linkedList.add(randomGenerator.nextInt(0, linkedList.size() - 1), num);
+        return num;
+    }
+
+    @Benchmark
+    public Integer linkedListTraverseAndInsert() {
+        int num = randomGenerator.nextInt(100, 1000);
+        int position = randomGenerator.nextInt(linkedList.size());
+
+        ListIterator<Integer> it = linkedList.listIterator();
+        for (int i = 0; i < position + 1; i++) {
+            it.next();
+        }
+        it.add(num);
+
+        return num;
+    }
+
+    // =============== REMOVE ====================
+    @Benchmark
+    public int linkedListRemoveFirst() {
+        return linkedList.removeFirst();
+    }
+
+    @Benchmark
+    public int linkedListRemove() {
+        int position = randomGenerator.nextInt(linkedList.size());
+        return linkedList.remove(position);
     }
 
     // =============== RANDOM ACCESS ====================
-
     @Benchmark
     public int linkedListRandomAccess() {
-        return linkedList.get(random.nextInt(size));
+        return linkedList.get(randomGenerator.nextInt(size));
     }
 
     // =============== ITERATION ====================
-
     @Benchmark
     public long linkedListIteration() {
         long sum = 0;
